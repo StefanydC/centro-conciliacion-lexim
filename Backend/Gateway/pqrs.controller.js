e57949098a5b8cb6b0c4ee70fae81697831ccadd
Stefany Caceres <stefanydctg12@gmail.com>
2026-08-01 10:26:12 -0500

    cambio de Ipv6 a IPv4

--- Backend/Gateway/pqrs.controller.js
+++ Backend/Gateway/pqrs.controller.js
@@ -30,9 +30,15 @@ const upload = multer({
 // Configuración de Nodemailer usando tus variables del .env
 const transporter = nodemailer.createTransport({
     service: 'gmail',
+    port: 587,
+    secure: false,
+    family: 4,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS  
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
